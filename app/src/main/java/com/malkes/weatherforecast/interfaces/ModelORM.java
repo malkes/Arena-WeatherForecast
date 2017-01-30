965e9ec1aa7bc38def347524c4fa71b99da59bc0
Leonardo Malkes <leo.malkes@madeinweb.com.br>
2017-01-30 02:50:02 -0300

    Remove unused code

--- app/src/main/java/com/malkes/weatherforecast/interfaces/ModelORM.java
+++
@@ -1,9 +0,0 @@
-package com.malkes.weatherforecast.interfaces;
-
-/**
- * Created by Malkes on 28/01/17.
- */
-
-public interface ModelORM {
-    Long getId();
-}
--- app/src/main/java/com/malkes/weatherforecast/util/ImageUtil.java
+++ app/src/main/java/com/malkes/weatherforecast/util/ImageUtil.java
@@ -15,27 +15,6 @@ import static android.R.attr.data;
  */
 
 public class ImageUtil {
-    private static final float BITMAP_SCALE = 0.4f;
-    private static final float BLUR_RADIUS = 7.5f;
-
-    public static Bitmap blur(Context ctx, Bitmap image) {
-        int width = Math.round(image.getWidth() * BITMAP_SCALE);
-        int height = Math.round(image.getHeight() * BITMAP_SCALE);
-
-        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
-        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
-
-        RenderScript rs = RenderScript.create(ctx);
-        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
-        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
-        theIntrinsic.setRadius(BLUR_RADIUS);
-        theIntrinsic.setInput(tmpIn);
-        theIntrinsic.forEach(tmpOut);
-        tmpOut.copyTo(outputBitmap);
-
-        return outputBitmap;
-    }
 
     public static Drawable getDrawableByName(Context context,String name){
         int id = context.getResources().getIdentifier(name.replace("-","_"), "drawable", context.getPackageName());
